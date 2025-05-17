79a916e7c6ede0b777282392477f64d4f11e7ccd
xerikperez <xerikperez@gmail.com>
2025-05-17 19:03:56 -0400

    Auth config

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,14 +2,22 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { signIn } from "next-auth/react";
 
 export const Social = () => {
+  
+  
   return (
     <div className="flex justify-center items-center w-full gap-x-2">
       <Button size="lg" variant="outline" className="w-30" onClick={() => {}}>
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button size="lg" className="w-30" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-30"
+        variant="outline"
+        onClick={() => signIn("GitHub")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
