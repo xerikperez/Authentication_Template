b61fe14447960016220c344590a1f47d8c0a1f86
xerikperez <xerikperez@gmail.com>
2025-05-20 07:40:37 -0400

    about page

---
+++ app/(protected)/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page of our application.</p>
+    </div>
+  );
+}
+// This is a simple About page component for a Next.js application.
